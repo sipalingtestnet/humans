16bd01083a14eddb873df6b549571b1f0f01b6ed
ALRIZKI HAIKAL <115471452+sipalingtestnet@users.noreply.github.com>
2022-12-14 17:32:18 +0800

    Update humans.sh

--- humans.sh
+++ humans.sh
@@ -22,13 +22,4 @@ echo -e "\e[1m\e[32m1. Install packages... \e[0m" && sleep 1
 cd $HOME
 sudo apt update && sudo apt upgrade -y
 sudo apt install curl tar wget clang pkg-config libssl-dev jq build-essential bsdmainutils git make ncdu gcc git jq chrony liblz4-tool -y
-echo -e "\e[1m\e[32m4. InstallGo  \e[0m" && sleep 1
-ver="1.19" && \
-wget "https://golang.org/dl/go$ver.linux-amd64.tar.gz" && \
-sudo rm -rf /usr/local/go && \
-sudo tar -C /usr/local -xzf "go$ver.linux-amd64.tar.gz" && \
-rm "go$ver.linux-amd64.tar.gz" && \
-echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> $HOME/.bash_profile && \
-source $HOME/.bash_profile && \
-go version
 echo '--------------DONO----------------'
